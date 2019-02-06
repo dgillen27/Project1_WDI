67e6ab7d5d2398ffb700d2a3366a84e407ab5dd5
Daniel Gillen <danielgillen@ool-ad020694.dyn.optonline.net>
2019-02-05 19:53:50 -0500

    GAME WORKS

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const starterSquare = document.querySelector("#starter-square")
 const starterTriangle = document.querySelector("#starter-triangle")
 const starterCircle = document.querySelector("#starter-circle")
 const tutorialPage = document.querySelector("section")//tutorial pagerandomPosition
-let time = 10;
+let time = 30;
 
 //function that creates the shapes
 const createShapes = () => {
@@ -20,7 +20,6 @@ const createShapes = () => {
       shape.classList.add("shape");
       shape.classList.add(shapeSelection[Math.floor(Math.random() * shapeSelection.length)])
       main.appendChild(shape)
-      // debugger;
       setInterval(randomPosition, 1000, shape);
     }
 }
@@ -32,6 +31,7 @@ const randomPosition = (shape) => {
 
     shape.style.bottom = `${styleTop}px`;
     shape.style.left = `${styleLeft}px`;
+    youWon();
 }
 //Function to start the game
 const startGame = () => {
@@ -43,74 +43,53 @@ const startGame = () => {
 //start button/game starter
 button.addEventListener('click', startGame);
 
-//Timer function taken from stackoverflow
-// function startTimer(duration, display) {
-//     var timer = duration, minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10)
-//         seconds = parseInt(timer % 60, 10);
-//
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-//         display.textContent = minutes + ":" + seconds;
-//
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
-//     dur = duration;
-// }
-
-
 //Timer.com
-
 const startTimer = () => {
-  display.innerText = time--
+  display.innerText = time--;
 }
-//Core game
 
+//Core game
 const changeShape = (ev) => {
  if (ev.target.className === "shape square"){
    ev.target.classList.add('triangle');
    ev.target.classList.remove("square");
  }
  else if (ev.target.className === "shape triangle") {
-   ev.target.classList.add('circle')
-   ev.target.classList.remove('triangle')
+   ev.target.classList.add('circle');
+   ev.target.classList.remove('triangle');
  }
  else if (ev.target.className === "shape circle") {
-   ev.target.classList.add("square")
-   ev.target.classList.remove("circle")
+   ev.target.classList.add("square");
+   ev.target.classList.remove("circle");
  }
- youWon();
 }
-
 
 const getRid = () => {
     document.querySelectorAll(".shape").forEach( (el) => {
       el.remove();
+        display.remove();
     })
   }
-//win condition
 
+//win condition
 const youWon = () => {
-  const madShapes = document.querySelectorAll(".shape")
+  const madShapes = document.querySelectorAll(".shape");
   if (document.querySelectorAll(".square").length === 12 && time > 0) {
-    title.innerText = "SQUARES WIN"
+    title.innerText = "SQUARES WIN";
       setTimeout(getRid, 300);
   } else if (document.querySelectorAll(".triangle").length === 12 && time > 0) {
-    title.innerText = "TRIANGLES WIN"
+    title.innerText = "TRIANGLES WIN";
       setTimeout(getRid, 300);
   } else if (document.querySelectorAll(".circle").length === 12 && time > 0) {
-    title.innerText = "CIRCLES WIN"
+    title.innerText = "CIRCLES WIN";
       setTimeout(getRid, 300);
-  }else if (time <= 0) {
-    alert("you lost")
+  } else if (time === 0) {
+    console.log("you Lost");
+      getRid();
   } else {
-    title.innerText = "Click Those Shapes!"
+    title.innerText = "Click Those Shapes!";
   }
 }
 
 //change shape function
-
 main.addEventListener('click', changeShape);
