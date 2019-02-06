b3f6b5c8a748cbaf2eee0585b97a9677976d3989
Daniel Gillen <danielgillen@ool-ad020694.dyn.optonline.net>
2019-02-05 19:09:13 -0500

    made a functioning timer, linked win condition but not lose condition

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const placeTime = document.querySelector("#clock")
 const starterSquare = document.querySelector("#starter-square")
 const starterTriangle = document.querySelector("#starter-triangle")
 const starterCircle = document.querySelector("#starter-circle")
-const tutorialPage = document.querySelector("section")//tutorial page
+const tutorialPage = document.querySelector("section")//tutorial pagerandomPosition
+let time = 10;
 
 //function that creates the shapes
 const createShapes = () => {
@@ -19,79 +20,91 @@ const createShapes = () => {
       shape.classList.add("shape");
       shape.classList.add(shapeSelection[Math.floor(Math.random() * shapeSelection.length)])
       main.appendChild(shape)
-      randomPosition(shape);
+      // debugger;
+      setInterval(randomPosition, 1000, shape);
     }
 }
-const randomPosition = (thing) => {
+
+
+const randomPosition = (shape) => {
   const styleTop = Math.floor(Math.random() * (window.innerHeight - 200));
   const styleLeft = Math.floor(Math.random() * (window.innerWidth - 100));
 
-  thing.style.bottom = `${styleTop}px`;
-  thing.style.left = `${styleLeft}px`;
+    shape.style.bottom = `${styleTop}px`;
+    shape.style.left = `${styleLeft}px`;
 }
 //Function to start the game
 const startGame = () => {
-  startTimer(30, display);
     tutorialPage.remove();
       createShapes();
+        setInterval(startTimer, 1000)
 }
 
 //start button/game starter
 button.addEventListener('click', startGame);
 
 //Timer function taken from stackoverflow
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10)
+//         seconds = parseInt(timer % 60, 10);
+//
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+//         display.textContent = minutes + ":" + seconds;
+//
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+//     dur = duration;
+// }
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+//Timer.com
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+const startTimer = () => {
+  display.innerText = time--
 }
-
-
-
 //Core game
 
 const changeShape = (ev) => {
  if (ev.target.className === "shape square"){
    ev.target.classList.add('triangle');
    ev.target.classList.remove("square");
-   console.log("you clicked a square")
  }
  else if (ev.target.className === "shape triangle") {
    ev.target.classList.add('circle')
    ev.target.classList.remove('triangle')
-   console.log("you clicked a triangle");
  }
  else if (ev.target.className === "shape circle") {
    ev.target.classList.add("square")
    ev.target.classList.remove("circle")
-   console.log("you clicked a cirlce");
  }
  youWon();
 }
 
+
+const getRid = () => {
+    document.querySelectorAll(".shape").forEach( (el) => {
+      el.remove();
+    })
+  }
 //win condition
 
 const youWon = () => {
   const madShapes = document.querySelectorAll(".shape")
-
-  if (document.querySelectorAll(".square").length === 12) {
+  if (document.querySelectorAll(".square").length === 12 && time > 0) {
     title.innerText = "SQUARES WIN"
-  } else if (document.querySelectorAll(".triangle").length === 12) {
+      setTimeout(getRid, 300);
+  } else if (document.querySelectorAll(".triangle").length === 12 && time > 0) {
     title.innerText = "TRIANGLES WIN"
-  } else if (document.querySelectorAll(".circle").length === 12) {
+      setTimeout(getRid, 300);
+  } else if (document.querySelectorAll(".circle").length === 12 && time > 0) {
     title.innerText = "CIRCLES WIN"
-  }else if (placeTime === 0) {
+      setTimeout(getRid, 300);
+  }else if (time <= 0) {
     alert("you lost")
   } else {
     title.innerText = "Click Those Shapes!"
