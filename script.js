ecb6b346d76fff122923fb69a85bfac79064b12a
Daniel Gillen <danielgillen@ool-ad020694.dyn.optonline.net>
2019-02-06 11:07:40 -0500

    it works

--- script.js
+++ script.js
@@ -10,45 +10,44 @@ const starterSquare = document.querySelector("#starter-square")
 const starterTriangle = document.querySelector("#starter-triangle")
 const starterCircle = document.querySelector("#starter-circle")
 const tutorialPage = document.querySelector("section")//tutorial pagerandomPosition
-let time = 30;
+let time = 5;
+// const square = document.querySelector('shape square')
+// const triangle = document.querySelector('shape triangle')
+// const circle = document.querySelector('shape circle')
 
-//function that creates the shapes
+
 const createShapes = () => {
     for(let i = 0; i < 12; i++) {
       const shape = document.createElement("div");
       shape.id = "shapes" + i;
       shape.classList.add("shape");
-      shape.classList.add(shapeSelection[Math.floor(Math.random() * shapeSelection.length)])
-      main.appendChild(shape)
+      shape.classList.add(shapeSelection[Math.floor(Math.random() * shapeSelection.length)]);
+      main.appendChild(shape);
       setInterval(randomPosition, 1000, shape);
     }
 }
 
-
 const randomPosition = (shape) => {
-  const styleTop = Math.floor(Math.random() * (window.innerHeight - 200));
+  const styleBottom = Math.floor(Math.random() * (window.innerHeight - 200));
   const styleLeft = Math.floor(Math.random() * (window.innerWidth - 100));
 
-    shape.style.bottom = `${styleTop}px`;
+    shape.style.bottom = `${styleBottom}px`;
     shape.style.left = `${styleLeft}px`;
-    youWon();
 }
-//Function to start the game
+
 const startGame = () => {
     tutorialPage.remove();
-      createShapes();
-        setInterval(startTimer, 1000)
-}
+    createShapes();
+    setInterval(startTimer, 1000);
+};
 
-//start button/game starter
 button.addEventListener('click', startGame);
 
-//Timer.com
 const startTimer = () => {
   display.innerText = time--;
-}
+  endGame();
+};
 
-//Core game
 const changeShape = (ev) => {
  if (ev.target.className === "shape square"){
    ev.target.classList.add('triangle');
@@ -67,25 +66,40 @@ const changeShape = (ev) => {
 const getRid = () => {
     document.querySelectorAll(".shape").forEach( (el) => {
       el.remove();
-        display.remove();
+      display.remove();
     })
   }
 
-//win condition
-const youWon = () => {
+const winPage = () => {
+  let highestTimeoutId = setTimeout(";");
+  for (let i = 0; i < highestTimeoutId; i++) {
+    clearTimeout(i);
+  }
+  alert("YOU WON")
+}
+
+const losePage = () => {
+  let highestTimeoutId = setTimeout(";");
+  for (let i = 0; i < highestTimeoutId; i++) {
+    clearTimeout(i);
+  }
+  alert("YOU LOST")
+}
+
+const endGame = () => {
   const madShapes = document.querySelectorAll(".shape");
   if (document.querySelectorAll(".square").length === 12 && time > 0) {
-    title.innerText = "SQUARES WIN";
-      setTimeout(getRid, 300);
+    document.createElement("h1")
+      winPage();
+      console.log("you won");
   } else if (document.querySelectorAll(".triangle").length === 12 && time > 0) {
-    title.innerText = "TRIANGLES WIN";
-      setTimeout(getRid, 300);
+      winPage();
+      console.log("you won");
   } else if (document.querySelectorAll(".circle").length === 12 && time > 0) {
-    title.innerText = "CIRCLES WIN";
-      setTimeout(getRid, 300);
-  } else if (time === 0) {
-    console.log("you Lost");
-      getRid();
+      winPage();
+      console.log("you won");
+  } else if (time < 0) {
+      losePage();
   } else {
     title.innerText = "Click Those Shapes!";
   }
